818845218e7fcea363acd2a06a7e49fbc32e233e
Fabrice Toussaint <frftoussaint@gmail.com>
2020-11-19 16:03:31 +0100

    Created prefect-poc for Omdena.

--- flow.py
+++ flow.py
@@ -1,75 +1,20 @@
 import os
 
-import prefect
 from prefect import Flow, Parameter
-from prefect.engine.results import LocalResult
-from prefect.engine.executors import LocalDaskExecutor, LocalExecutor
-from prefect.utilities.edges import unmapped
+from tasks import generate_rgbd
+from datetime import timedelta
+from prefect.schedules import IntervalSchedule
 
-from tasks import postgres, socrata
-
-
-"""
-Flow: Loading Socrata Data to Postgres
---------------------------------------
-This flow downloads data from the Socrata Open Data site and loads it
-into a Postgres database.
-
-Behavior is configured in the config.toml
-"""
+schedule = IntervalSchedule(interval=timedelta(hours=24))
 
 with Flow(
-        'Loading Socrata data to Postgres',
-        result=LocalResult(
-            dir=os.path.join(os.path.dirname(__file__), "results"),
-            validate_dir=True
-            ),
-        state_handlers=[postgres.log_to_database]
+        'Prefect Omdena POC',
+        schedule
 ) as flow:
+    args = {'artifact': 'pc_1583742419-7h4mpmc587_1591451354739_100_000.pcd',
+            'base_data_path': '/usr/local/airflow/dags/data'
+            }
 
-    datasets = Parameter("datasets")
+    generate_rgbd.fuse_into_rgbd(artifact=args["artifact"], base_data_path=args["base_data_path"])
 
-    # get last updated from database
-    since = postgres.get_last_updated()
-    # download dataset from Socrata
-    downloads = socrata.download_dataset.map(
-        dataset=datasets,
-        since=unmapped(since)
-    )
-    # get the temp tables ready for load
-    prep = postgres.prep_load()
-    # load each downloaded file
-    load = postgres.load_datafile.map(
-        datafile=downloads
-    )
-    # commit new data to database and clean up
-    complete = postgres.complete_load()
-
-    # make sure prep runs before load
-    flow.add_edge(upstream_task=prep, downstream_task=load)
-    # make sure load runs before complete
-    flow.add_edge(upstream_task=load, downstream_task=complete)
-
-
-if __name__ == "__main__":
-    logger = prefect.context.get("logger")
-
-    dask = prefect.config.dask
-    mode = prefect.config.mode
-    reset_db = prefect.config.reset_db
-
-    all_datasets = dict(prefect.config.socrata.datasets)
-    years = list(prefect.config.data.years)
-
-    # use only year datasets if in full mode otherwise use all w/since
-    if mode == 'full':
-        run_datasets = dict((k, all_datasets[k]) for k in years)
-    else:
-        run_datasets = all_datasets
-
-    logger.info(f"Starting \"{mode}\" flow for {', '.join(run_datasets.keys())}"
-                f" {'and resetting db' if reset_db else ''}")
-    state = flow.run(
-        datasets=list(run_datasets.values()),
-        executor=LocalDaskExecutor() if dask else LocalExecutor()
-    )
+flow.run()
---
+++ tasks/generate_rgbd.py
@@ -0,0 +1,38 @@
+from PIL import Image, ImageOps
+import numpy as np
+import pandas as pd
+import pickle
+import os
+from prefect import task, Flow
+
+@task
+def fuse_into_rgbd(artifact=None, base_data_path=None, **kwargs):
+    print(kwargs)
+    print(artifact)
+    print(base_data_path)
+
+    qrcode = artifact.split('_')[1]
+    depth_path = base_data_path + '/depth/omdena_datasets/depthmap_training_dataset/scans/' + qrcode + '/100'
+    rgb_path = base_data_path + '/rgb/omdena_datasets/rgb_training/scans/' + qrcode + '/100'
+    output_path = base_data_path + '/rgbd/' + qrcode + '/100'
+
+    with open(depth_path + '/' + artifact[:-2], 'rb') as f:
+        depthmap, label = pickle.load(f)
+    depthmap = depthmap.squeeze()
+
+    metadata = pd.read_csv(base_data_path + '/pcd-rgb-metadata.csv')
+    closest_rgb = metadata[metadata.artifact == artifact[:-2]].closest_rgb.values[0]
+
+    im = Image.open(rgb_path + '/' + closest_rgb)
+    im = im.resize(depthmap.shape)
+    im = im.rotate(-90, expand=True)
+    im = ImageOps.mirror(im)
+
+    rgb = np.array(im)
+    rgbd = np.concatenate([rgb, depthmap[..., np.newaxis]], axis=2)
+
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    with open(output_path + '/' + artifact[:-4] + '.rgbd', 'wb') as f:
+        pickle.dump(rgbd, f)
